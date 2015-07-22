1ce3b3e7d80367dcc5d1a826040700ea8747db3f
dade3007 <dade3007@gmail.com>
2015-07-22 22:58:11 +0200

    menu-toggle
    menu toggle for mobile version

--- war/main/js/main.js
+++ war/main/js/main.js
@@ -2,11 +2,20 @@ $(function() {
 	
 	$('#title-container').addClass("final");
 	
+	
+	
+	
 	setTimeout(function() { $('#section-about').addClass("final")}, 250);
 });
 
 
+$('#menu-toggle').click(function(){
+	$('#sidebar-wrapper').addClass("active");
+});
 
+$("#menu-close").click(function(){
+	$('#sidebar-wrapper').removeClass("active");
+});
 
 $('a').click(function(){
     $('html, body').animate({
